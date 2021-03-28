867fb007e7a125ab713b674088bd8732e35ed38e
Max MacMillan <shannon@bluestormcreative.com>
2021-03-27 23:22:22 -0700

    Clean up nav spacing

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Nav = () => (
-	<nav className="site-nav border-b-2 border-solid border-gray-400">
-		About
-		Portfolio
-		Resume
-		Contact
+	<nav className="site-nav border-b-2 border-solid border-gray-400 py-4 flex space-x-4">
+		<a className="nav-item inline-block" href="#about">About</a>
+		<span className="nav-item inline-block">Portfolio</span>
+		<span className="nav-item inline-block">Resume</span>
+		<span className="nav-item inline-block">Contact</span>
 	</nav>
 );
 
