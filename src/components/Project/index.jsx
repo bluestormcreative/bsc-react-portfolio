e42c8f63dbcda561236a5e11c604a2a318345377
Max MacMillan <shannon@bluestormcreative.com>
2021-03-28 13:29:22 -0700

    Loop through project data and print

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -4,12 +4,29 @@ const Project = ({ project }) => {
   const {
     name,
     cat,
+    desc,
+    image,
+    link,
   } = project;
+
+  const importAll = (r) => {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  const allImages = importAll(require.context('../../assets/project-images', false, /\.(png|jpe?g|svg)$/));
 
   return (
     <div className="project">
+      <div className="project__cats text-xs">
+        {cat && cat.map((cat) => (
+          <span className="project__cat">{cat}</span>
+        ))}
+      </div>
       <h2 className="project__name text-xl">{name}</h2>
-      {cat}
+      <p className="project__desc">{desc}</p>
+      <img src={allImages[`${image}`].default} alt={`${name} project screenshot`} />
     </div>
   );
 };
