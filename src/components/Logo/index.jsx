23d231503d8e3c7b234b7264ee9339b60cc061bc
Max MacMillan <shannon@bluestormcreative.com>
2021-03-27 21:29:56 -0700

    Stub out basic layout

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => (
+	<div className="logo-container">
+		logo here
+	</div>
+);
+
+export default Logo;
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => (
+	<div className="nav">
+		About
+		Categories 1
+		Categories 2
+		Categories 3
+		Contact
+	</div>
+);
+
+export default Nav;
