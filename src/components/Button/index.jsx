37bb4485eb97ef9aba029af2bbf89500fd82ff26
Max MacMillan <shannon@bluestormcreative.com>
2021-03-28 15:31:22 -0700

    Add type to buttons

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Button = ({ isLink, classNames, newTab, link, text, buttonClick }) => {
+const Button = ({ isLink, classNames, newTab, link, text, buttonClick, type = "button" }) => {
   const classes = `${classNames} inline-block bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 my-4 border border-blue-500 hover:border-transparent`;
   const target = newTab !== false ? 'blank' : '';
 
@@ -20,6 +20,7 @@ const Button = ({ isLink, classNames, newTab, link, text, buttonClick }) => {
     <button
       className={classes}
       onClick={buttonClick}
+      type={type}
     >
       {text}
     </button>
